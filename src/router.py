31d342d0c0cd130e423e328e2fbb76e5357b873e
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2019-05-16 07:55:08 -0300

    Prompt de comando aceita comandos add, del e link. Suporte a batch.

--- src/router.py
+++ src/router.py
@@ -10,11 +10,15 @@ import sys
 # ======================
 EXIBIR_LOG         = True
 PORTA              = 55151
+ARGS_INSUFICIENTES = "Erro com o comando {0}. Argumentos insuficientes."
+CMD_NAOENCONTRADO  = "Comando não encontrado: {0}"
+PROMPT             = "$ "
 
 # VARIÁVEIS DO PROGRAMA
 # =====================
-parametros = {'periodo' : 0, 'ip': '127.0.0.1', 'partida': ''}
-
+parametros = {'periodo' : 0, 'ip': '127.0.0.1', 'topologia': ''}
+#distancia = {'prox': '0.0.0.0', 'peso': 0}
+distancias = {}
 
 # FUNCOES DO PROGRAMA
 # ===================
@@ -22,6 +26,7 @@ def log(msg):
   if EXIBIR_LOG:
     print(msg) 
 
+# -----------------------------------------
 # Lê os argumentos do programa
 # arg1 = ip
 # arg2 = período de repetição da msg
@@ -32,25 +37,89 @@ def args_processar(parametros):
   if len(sys.argv) > 3:
     parametros['topologia'] = sys.argv[3]
 
+# -----------------------------------------  
+def entrada_carregar(nomearquivo):
+  arquivo = open(nomearquivo, "r")
+  linhas = arquivo.read().splitlines()
+  for linha in linhas:
+    yield linha
+
+# -----------------------------------------
+def distancias_adicionar(ip, distancia):
+  distancias[ip] = distancia
+  
+def distancias_remover(ip):
+  del distancias[ip]
+  
+def distancias_obterpeso(ip):
+  return distancias[ip]['peso']
+
+def distancias_obterprox(ip):
+  return distancias[ip]['prox']
+
+# -----------------------------------------
+def cmd_add(args):
+  if len(args) > 2:
+    distancia = {'prox': args[1], 'peso': args[2]}
+    distancias_adicionar(args[1], distancia)
+  else:
+    print(ARGS_INSUFICIENTES.format(args[0]))
+
+def cmd_del(args):
+  if len(args) > 1:
+    distancias_remover(args[1])
+  else:
+    print(ARGS_INSUFICIENTES.format(args[0]))
+
+def cmd_quit(args):
+  sys.exit()
+  
+def cmd_links(args):
+  print("IP        PESO     PROXIMO")
+  print("==========================")
+  for ip in distancias.keys():
+    print(ip, distancias_obterpeso(ip), "     ", distancias_obterprox(ip))
+  
+cmds_disponiveis = {
+  "add": cmd_add,
+  "del": cmd_del,
+  "quit": cmd_quit,
+  "links": cmd_links
+}
+
+# -----------------------------------------
+def cmdline_batch(cmds):
+  for cmd in cmds:
+    print(PROMPT + cmd)
+    cmdline_executar(cmd)
+
+def cmdline_executar(cmd):
+  cmd_args = cmd.split(' ')
+  if cmd_args[0] in cmds_disponiveis.keys():
+    cmds_disponiveis[cmd_args[0]](cmd_args)
+  else:
+    print(CMD_NAOENCONTRADO.format(cmd_args[0]))
+
 def cmdline_obter():
   s = ''
   while len(s) == 0:
     try:
-      s = input('$ ')
+      s = input(PROMPT)
     except:
       sys.exit()
   
-  return s.strip()
-  
+  return ' '.join(s.split())
+
 def cmdline_manipular():
   while (True):
     cmd = cmdline_obter()
-    if (cmd == 'quit'):
-      sys.exit()
+    cmdline_executar(cmd)
 
 # CORPO DO PROGRAMA
 # =================
 if len(sys.argv) > 2:
   args_processar(parametros)
-  
+  if len(parametros['topologia']) > 0:
+    comandos = entrada_carregar(parametros['topologia'])
+    cmdline_batch(comandos)
   cmd = cmdline_manipular()
