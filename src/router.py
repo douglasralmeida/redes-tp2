e4774701364913c3943b3ad4de8a19148c2c68ef
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2019-05-18 15:56:34 -0300

    Adiciona threads para deixar o programa mais reabilitável

--- src/router.py
+++ src/router.py
@@ -4,7 +4,8 @@
 # TP2 de Redes
 
 import socket
-import sys
+import sys 
+import threading
 
 # CONSTANTES DO PROGRAMA
 # ======================
@@ -12,13 +13,84 @@ EXIBIR_LOG         = True
 PORTA              = 55151
 ARGS_INSUFICIENTES = "Erro com o comando {0}. Argumentos insuficientes."
 CMD_NAOENCONTRADO  = "Comando não encontrado: {0}"
+CONEX_NAOREALIZADA = "A conexão com o endereço {0} não foi realizada."
 PROMPT             = "$ "
 
 # VARIÁVEIS DO PROGRAMA
 # =====================
-parametros = {'periodo' : 0, 'ip': '127.0.0.1', 'topologia': ''}
+parametros = {'periodo' : 0, 'ip': '127.0.0.1', 'topologia': '', 'porta': 0, 'timeout': 7}
 #distancia = {'prox': '0.0.0.0', 'peso': 0}
 distancias = {}
+enlaces = []
+threads = []
+redethread = 0
+
+# CLASSES DO PROGRAMA
+# ===================
+# Gerencia um enlace
+class EnlaceThread(threading.Thread):
+  def __init__(self, soquete, ip):
+    threading.Thread.__init__(self)
+    self.soquete = soquete
+    self.ip = ip
+    enlaces.append(ip)
+    log("[+] Nova thread para o enlance ao endereço " + ip + ".")
+
+  # manipular o self.soquete para receber dados do enlace
+  def run(self):
+    while(True):
+      try:
+        dados = self.soquete.recv()
+      except socket.timeout:
+        continue
+      if not dados:
+        break
+      if dados == '':
+        break
+      
+      # recebeu uma mensagen
+      self.onrecv(client, dados)
+   
+    # removendo cliente da listas de enlaces
+    enlaces.remove(ip)
+    log("[-] Encerrando thread do enlance ao endereço " + ip + ".")
+    
+    # desconectando...
+    sel.soquete.close()
+    
+    # encerrando thread
+    thread.exit()
+    threads.remove(self)
+    
+  def onrecv(self):
+    pass
+
+# Cria enlaces com a rede passivamente
+class RedeThread(threading.Thread):
+  def __init__(self, soquete):
+    threading.Thread.__init__(self)
+    self.escutarrede = True
+    self.soquete = soquete
+    self.soquete.listen(4)
+    self.soquete.settimeout(1)
+
+  # manipular o soquete para formar enlaces
+  def run(self):
+    log("[O] Aguardando por novas conexões...")
+    while self.escutarrede:
+      try:
+        (cliente, (ip, porta)) = self.soquete.accept()
+      except socket.timeout:
+        continue
+      if not self.escutarrede:
+        break
+      enlacethread = EnlaceThread(cliente, ip)
+      enlacethread.start()
+      threads.append(enlacethread)
+    log("[O] Encerrando escuta por novas conexões...")
+      
+  def desligar(self):
+    self.escutarrede = False
 
 # FUNCOES DO PROGRAMA
 # ===================
@@ -36,6 +108,7 @@ def args_processar(parametros):
   parametros['periodo'] = int(sys.argv[2])
   if len(sys.argv) > 3:
     parametros['topologia'] = sys.argv[3]
+  parametros['porta'] = PORTA
 
 # -----------------------------------------  
 def entrada_carregar(nomearquivo):
@@ -60,8 +133,16 @@ def distancias_obterprox(ip):
 # -----------------------------------------
 def cmd_add(args):
   if len(args) > 2:
-    distancia = {'prox': args[1], 'peso': args[2]}
-    distancias_adicionar(args[1], distancia)
+    ip = args[1]
+    distancia = {'prox': ip, 'peso': args[2]}
+    soquete = rede_conectar((ip, parametros['porta']))
+    if soquete is None:
+      print(CONEX_NAOREALIZADA.format(ip))
+      return
+    enlacethread = EnlaceThread(soquete, ip)
+    enlacethread.start()
+    threads.append(enlacethread)    
+    distancias_adicionar(ip, distancia)
   else:
     print(ARGS_INSUFICIENTES.format(args[0]))
 
@@ -72,14 +153,14 @@ def cmd_del(args):
     print(ARGS_INSUFICIENTES.format(args[0]))
 
 def cmd_quit(args):
-  sys.exit()
+  app_sair()
   
 def cmd_links(args):
   print("IP        PESO     PROXIMO")
   print("==========================")
   for ip in distancias.keys():
     print(ip, distancias_obterpeso(ip), "     ", distancias_obterprox(ip))
-  
+
 cmds_disponiveis = {
   "add": cmd_add,
   "del": cmd_del,
@@ -106,7 +187,7 @@ def cmdline_obter():
     try:
       s = input(PROMPT)
     except:
-      sys.exit()
+      app_sair()
   
   return ' '.join(s.split())
 
@@ -115,11 +196,46 @@ def cmdline_manipular():
     cmd = cmdline_obter()
     cmdline_executar(cmd)
 
+# -----------------------------------------
+def app_sair():
+  for t in threads:
+    t.join()
+  redethread.desligar()
+  redethread.join()
+  sys.exit()
+  
+# -----------------------------------------
+def rede_iniciar(param):
+  global redethread
+  
+  # Cria um soquete para comunicação externa
+  soquete = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+  # Para evitar a exceção "address already in use",
+  # desligar esse comportamento com uma opção da API de soquetes:
+  soquete.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+  soquete.bind(param)
+
+  # Inicia a thread que irá escutar por novas conexões
+  redethread = RedeThread(soquete)
+  redethread.start()
+
+def rede_conectar(param):
+  log("[O] Conectando ativamente com {0}...".format(param[0]))
+  try:
+    soquete = socket.create_connection(param, parametros['timeout'])
+  except:
+    soquete = None
+  
+  return soquete
+
 # CORPO DO PROGRAMA
 # =================
 if len(sys.argv) > 2:
   args_processar(parametros)
+  rede_iniciar((parametros['ip'], parametros['porta']))
   if len(parametros['topologia']) > 0:
     comandos = entrada_carregar(parametros['topologia'])
     cmdline_batch(comandos)
   cmd = cmdline_manipular()
+  app_sair()
